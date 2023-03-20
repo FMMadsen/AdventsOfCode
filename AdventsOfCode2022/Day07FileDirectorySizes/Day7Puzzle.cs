6b0b1a05d397183f564b32aa4fb3fa66a784d93d
Flemming Madsen <f.m.madsen@live.dk>
2023-03-20 02:11:56 +0100

    Day 7 part 1

---
+++ AdventsOfCode2022/Day07FileDirectorySizes/Day7Puzzle.cs
@@ -0,0 +1,54 @@
+﻿namespace AdventsOfCode2022.Day07FileDirectorySizes
+{
+    internal class Day7Puzzle
+    {
+        internal static string SolvePart1(string[] datasetLines, bool doPrintOut)
+        {
+            Terminal terminal = new Terminal();
+
+            foreach(string line in datasetLines)
+            {
+                if(line.StartsWith('$'))
+                {
+                    string command = line.Substring(2);
+
+                    if(command.ToLower().StartsWith("cd"))
+                    {
+                        terminal.ChangeDirectory(command);
+                    }
+                }
+                else
+                {
+                    terminal.CreateChildItems(line);
+                }
+            }
+
+            terminal.CalculateFileAndDirectorySizes();
+
+            var resultList = terminal.SearchDirectory(100000);
+
+            if (doPrintOut)
+            {
+                terminal.PrintFilesystem();
+
+                foreach(var result in resultList)
+                {
+                    Console.WriteLine($"{result.Path}");
+                }
+            }
+
+            int response = 0;
+            foreach (var result in resultList)
+            {
+                response += result.DirectorySize;
+            }
+
+            return response.ToString();
+        }
+
+        internal static string SolvePart2(string[] datasetLines, bool doPrintOut)
+        {
+            return "";
+        }
+    }
+}
---
+++ AdventsOfCode2022/Day07FileDirectorySizes/Terminal.cs
@@ -0,0 +1,82 @@
+﻿namespace AdventsOfCode2022.Day07FileDirectorySizes
+{
+    internal class Terminal
+    {
+        public Terminal()
+        {
+            RootDirectory = new DeviceDirectory(parent: null, "/");
+            CurrentDirectory = RootDirectory;
+        }
+
+        public DeviceDirectory RootDirectory { get; set; }
+
+        public DeviceDirectory CurrentDirectory { get; set; }
+
+        public void ChangeDirectory(string command)
+        {
+            if(command.ToLower().StartsWith("cd"))
+            {
+                string directoryName = command[3..].Trim();
+                
+                if(directoryName.Equals(".."))
+                {
+                    if(CurrentDirectory.Parent != null)
+                        CurrentDirectory = CurrentDirectory.Parent;
+                }
+                else
+                {
+                    var childDirectory = CurrentDirectory.GetChildDirectory(directoryName);
+                    if (childDirectory != null)
+                        CurrentDirectory = childDirectory;
+                }
+            }
+        }
+
+        public void CreateChildItems(string childItemLine)
+        {
+            if (childItemLine.ToLower().StartsWith("dir"))
+            {
+                string directoryName = childItemLine[4..].Trim();
+                CreateNewDirectory(directoryName);
+            }
+            else
+            {
+                CreateNewFile(childItemLine);
+            }
+        }
+
+        public void CreateNewDirectory(string directoryName)
+        {
+            DeviceDirectory newDirectory = new DeviceDirectory(CurrentDirectory, directoryName);
+            CurrentDirectory.Add(newDirectory);
+        }
+
+        public void CreateNewFile(string fileSizeNameString)
+        {
+            string[] fileSizeName = fileSizeNameString.Split(' ');
+            int fileSize = int.Parse(fileSizeName[0]);
+            string fileName = fileSizeName[1];
+            DeviceFile file = new DeviceFile(fileName, fileSize, CurrentDirectory);
+            CurrentDirectory.Add(file);
+        }
+
+        public void CalculateFileAndDirectorySizes()
+        {
+            RootDirectory.CalculateSize();
+        }
+
+        public void PrintFilesystem()
+        {
+            Console.WriteLine("---- DEVICE FILES ----");
+            RootDirectory.Print();
+            Console.WriteLine("----------------------");
+        }
+
+        public IList<DeviceDirectory> SearchDirectory(int maxSize)
+        {
+            var result = new List<DeviceDirectory>();
+            RootDirectory.SearchDirectories(maxSize, result);
+            return result;
+        }
+    }
+}
