e623a7eb77e58f842e1ed6603024e62140e78f79
Flemming Madsen <f.m.madsen@live.dk>
2023-12-05 19:06:41 +0100

    Framework code

--- AdventOfCode2023Solutions/Day05/Solution.cs
+++ AdventOfCode2023Solutions/Day05/Solution.cs
@@ -4,7 +4,7 @@ namespace AdventOfCode2023Solutions.Day05
 {
     public class Solution(string[] datasetLines) : IAOCSolution
     {
-        public string PuzzleName => "Day 5: ";
+        public string PuzzleName => "Day 5: If You Give A Seed A Fertilizer";
         public string[] DatasetLines => datasetLines;
 
         public string SolvePart1()
