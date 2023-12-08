21c2c82cc969ab21e77ee47fe773c76bbac1bc8a
Flemming Madsen <f.m.madsen@live.dk>
2023-12-08 10:14:49 +0100

    Added Day 8 unit tests

--- AdventOfCode2023Solutions/Day08/Solution.cs
+++ AdventOfCode2023Solutions/Day08/Solution.cs
@@ -4,7 +4,7 @@ namespace AdventOfCode2023Solutions.Day08
 {
     public class Solution(string[] datasetLines) : IAOCSolution
     {
-        public string PuzzleName => "Day 8: ";
+        public string PuzzleName => "Day 8: Haunted Wasteland";
         public string[] DatasetLines => datasetLines;
 
         public string SolvePart1()
