5bafaf86c47fee4de89b360443cdd9db53e412e0
Telpe <tommy.s.madsen@gmail.com>
2023-12-15 16:40:45 +0100

    Day 4 started

--- AdventOfCode2023Solutions/Day04/Solution.cs
+++ AdventOfCode2023Solutions/Day04/Solution.cs
@@ -7,15 +7,26 @@ namespace AdventOfCode2023Solutions.Day04
         public string PuzzleName => "Day 4: Scratchcards";
         public string[] DatasetLines => datasetLines;
 
+        public long Winnings = 0;
+
         public string SolvePart1()
         {
-            return "To be implemented";
+            Winnings = 0;
+
+            for (int lineNumber = 0; lineNumber < DatasetLines.Length; lineNumber++)
+            {
+                string[] card = DatasetLines[lineNumber].Split(':','|');
+            }
+
+
+            return Winnings.ToString();
         }
 
 
         public string SolvePart2()
         {
-            return "To be implemented";
+            Winnings = 0;
+            return Winnings.ToString();
         }
     }
 }
