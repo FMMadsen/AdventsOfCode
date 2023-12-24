a6d2081f2b4c84613ce34070e91a28baa78e3080
Flemming Madsen <f.m.madsen@live.dk>
2023-12-24 05:16:36 +0100

    Day 14 Part 2: Finished coding, but runs many hours

--- AdventOfCode2023Solutions/Day14/Solution.cs
+++ AdventOfCode2023Solutions/Day14/Solution.cs
@@ -6,10 +6,19 @@ namespace AdventOfCode2023Solutions.Day14
     {
         public string PuzzleName => "Day 14: Parabolic Reflector Dish";
 
+        public char[,]? PlatformInitial { get; set; } = null;
+        public char[,]? PlatformTiltetNorth { get; set; } = null;
+        public char[,]? PlatformTiltetWest { get; set; } = null;
+        public char[,]? PlatformTiltetSouth { get; set; } = null;
+        public char[,]? PlatformTiltetEast { get; set; } = null;
+        public char[,]? PlatformOneCircle { get; set; } = null;
+
+
         public string SolvePart1(string[] datasetLines)
         {
             var platform = LoadPlatform(datasetLines);
-            var stones = TiltPlatform(platform, out char[,] newTiltetNorthPlatform);
+            TiltPlatform(platform, out char[,] newTiltetNorthPlatform);
+            var stones = CalculateStonesWeight(newTiltetNorthPlatform);
             var sum = stones.Sum(c => c.Sum(s => s));
             return sum.ToString();
         }
@@ -17,7 +26,37 @@ namespace AdventOfCode2023Solutions.Day14
         public string SolvePart2(string[] datasetLines)
         {
             var platform = LoadPlatform(datasetLines);
-            var stones = TiltPlatform(platform, out char[,] newTiltetNorthPlatform);
+            PlatformInitial = platform;
+
+            char[,]? newTiltetNorthPlatform, newTiltetWestPlatform, newTiltetSouthPlatform, newTiltetEastPlatform, platformTransposed = null;
+
+            for (int i = 0; i < 1000000000; i++)
+            {
+                TiltPlatform(platform, out newTiltetNorthPlatform);
+
+                platformTransposed = TransposeMatrix90Degrees(newTiltetNorthPlatform);
+                TiltPlatform(platformTransposed, out newTiltetWestPlatform);
+
+                platformTransposed = TransposeMatrix90Degrees(newTiltetWestPlatform);
+                TiltPlatform(platformTransposed, out newTiltetSouthPlatform);
+
+                platformTransposed = TransposeMatrix90Degrees(newTiltetSouthPlatform);
+                TiltPlatform(platformTransposed, out newTiltetEastPlatform);
+
+                platformTransposed = TransposeMatrix90Degrees(newTiltetEastPlatform);
+
+                if (i % 10000 == 0)
+                    Console.WriteLine($"Counted {i:#,##0} out of 1.000.000.000");
+            }
+
+            var stones = CalculateStonesWeight(platformTransposed);
+
+            //PlatformTiltetNorth = newTiltetNorthPlatform;
+            //PlatformTiltetWest = newTiltetWestPlatform;
+            //PlatformTiltetSouth = newTiltetSouthPlatform;
+            //PlatformTiltetEast = newTiltetEastPlatform;
+            //PlatformOneCircle = platformTransposed;
+
             var sum = stones.Sum(c => c.Sum(s => s));
             return sum.ToString();
         }
@@ -60,11 +99,52 @@ namespace AdventOfCode2023Solutions.Day14
                             availableSpaceColumns[columnIndex].Enqueue(loadOrder);
                             break;
                     }
-
                 }
             }
-            newTiltetPlatform = new char[noOfRows, noOfCols];
+            newTiltetPlatform = MapOutPlatform(noOfRows, noOfCols, stoneColumns, rockColumns);
             return stoneColumns;
+        }
+
+        private static List<int>[] CalculateStonesWeight(char[,]? platform)
+        {
+            if (platform == null)
+                return new List<int>[0];
+
+            var noOfRows = platform.GetLength(0);
+            var noOfCols = platform.GetLength(1);
+
+            var stoneColumns = new List<int>[noOfCols];
+            for (int columnIndex = 0; columnIndex < noOfCols; columnIndex++)
+            {
+                stoneColumns[columnIndex] = new List<int>();
+
+                for (int rowIndex = 0; rowIndex < noOfRows; rowIndex++)
+                {
+                    var contentOnLocation = platform[rowIndex, columnIndex];
+                    var loadOrder = noOfRows - rowIndex;
+                    if (contentOnLocation == 'O')
+                        stoneColumns[columnIndex].Add(loadOrder);
+                }
+            }
+            return stoneColumns;
+        }
+
+        private static char[,] MapOutPlatform(int noOfRows, int noOfCols, List<int>[] stoneColumns, List<int>[] rockColumns)
+        {
+            var platform = new char[noOfRows, noOfCols];
+            for (int columnIndex = 0; columnIndex < noOfCols; columnIndex++)
+                for (int rowIndex = 0; rowIndex < noOfRows; rowIndex++)
+                    platform[rowIndex, columnIndex] = '.';
+
+            for (int c = 0; c < stoneColumns.Length; c++)
+                foreach (var stoneValue in stoneColumns[c])
+                    platform[noOfRows - stoneValue, c] = 'O';
+
+            for (int c = 0; c < rockColumns.Length; c++)
+                foreach (var rockValue in rockColumns[c])
+                    platform[noOfRows - rockValue, c] = '#';
+
+            return platform;
         }
 
         private static char[,] LoadPlatform(string[] datasetLines)
@@ -76,6 +156,20 @@ namespace AdventOfCode2023Solutions.Day14
                 for (int rowIndex = 0; rowIndex < noOfRows; rowIndex++)
                     platform[rowIndex, columnIndex] = datasetLines[rowIndex][columnIndex];
             return platform;
+        }
+
+        public static char[,] TransposeMatrix90Degrees(char[,] input)
+        {
+            var noOfYs = input.GetLength(0);
+            var noOfXs = input.GetLength(1);
+
+            var m = new char[noOfXs, noOfYs];
+
+            for (int y = 0; y < noOfYs; y++)
+                for (int x = 0; x < noOfXs; x++)
+                    m[x, noOfYs - 1 - y] = input[y, x];
+
+            return m;
         }
     }
 }
--- AdventOfCode2023UnitTests/Day14Tests.cs
+++ AdventOfCode2023UnitTests/Day14Tests.cs
@@ -26,8 +26,56 @@ namespace AdventOfCode2023UnitTests
             //act
             var result = solution.SolvePart2(dataset);
 
+            //print
+            //if (solution.PlatformInitial != null)
+            //    TestDataWriter.ReadDataSet(solution.PlatformInitial, $"Day14Part2_{DateTime.Now.ToString("yyMMdd-HHmmss")}_Initial.txt");
+            //if (solution.PlatformTiltetNorth != null)
+            //    TestDataWriter.ReadDataSet(solution.PlatformTiltetNorth, $"Day14Part2_{DateTime.Now.ToString("yyMMdd-HHmmss")}_Tiltet1North.txt");
+
+            //if (solution.PlatformTiltetWest != null)
+            //    TestDataWriter.ReadDataSet(solution.PlatformTiltetWest, $"Day14Part2_{DateTime.Now.ToString("yyMMdd-HHmmss")}_Tiltet2West.txt");
+            
+            //if (solution.PlatformTiltetSouth != null)
+            //    TestDataWriter.ReadDataSet(solution.PlatformTiltetSouth, $"Day14Part2_{DateTime.Now.ToString("yyMMdd-HHmmss")}_Tiltet3South.txt");
+            
+            //if (solution.PlatformTiltetEast != null)
+            //    TestDataWriter.ReadDataSet(solution.PlatformTiltetEast, $"Day14Part2_{DateTime.Now.ToString("yyMMdd-HHmmss")}_Tiltet4East.txt");
+
+            //if (solution.PlatformOneCircle != null)
+            //    TestDataWriter.ReadDataSet(solution.PlatformOneCircle, $"Day14Part2_{DateTime.Now.ToString("yyMMdd-HHmmss")}_OneCircle.txt");
+
             //assert
-            Assert.That(result, Is.EqualTo(""));
+            Assert.That(result, Is.EqualTo("64"));
+        }
+
+        [Test]
+        public void TransposeMatrix90Degrees()
+        {
+            //Prepare
+            char[,] input = new char[4, 2];
+            input[0, 0] = 'Y';
+            input[0, 1] = 'E';
+
+            input[1, 0] = 'R';
+            input[1, 1] = 'C';
+
+            input[2, 0] = 'E';
+            input[2, 1] = 'I';
+
+            input[3, 0] = 'V';
+            input[3, 1] = 'N';
+
+            //act
+            var result = AdventOfCode2023Solutions.Day14.Solution.TransposeMatrix90Degrees(input);
+            string string1 = $"{result[0, 0]}{result[0, 1]}{result[0, 2]}{result[0, 3]}";
+            string string2 = $"{result[1, 0]}{result[1, 1]}{result[1, 2]}{result[1, 3]}";
+
+            //assert
+            Assert.That(result.GetLength(0), Is.EqualTo(2));
+            Assert.That(result.GetLength(1), Is.EqualTo(4));
+
+            Assert.That(string1, Is.EqualTo("VERY"));
+            Assert.That(string2, Is.EqualTo("NICE"));
         }
     }
 }
