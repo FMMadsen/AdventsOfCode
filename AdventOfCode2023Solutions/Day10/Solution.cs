f2825264b9067d5ada7cb4edd6271a60d96daa46
Telpe <tommy.s.madsen@gmail.com>
2024-01-08 15:16:57 +0100

    Day 10 Done

--- AdventOfCode2023Solutions/Day10/Solution.cs
+++ AdventOfCode2023Solutions/Day10/Solution.cs
@@ -1,6 +1,8 @@
 ﻿using AdventOfCode2023Solutions.Day08;
 using Common;
+using System.Collections.Frozen;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Numerics;
 
 namespace AdventOfCode2023Solutions.Day10
@@ -117,12 +119,27 @@ namespace AdventOfCode2023Solutions.Day10
         {
             return HashCode.Combine(X, Y);
         }
+
+        
+        public PipeVector2 Right90()
+        {
+            return new PipeVector2 { X = -Y, Y = X };
+        }
+        public PipeVector2 Left90()
+        {
+            return new PipeVector2 { X = Y, Y = -X };
+        }
+        public PipeVector2 Back180()
+        {
+            return new PipeVector2 { X = -X, Y = -Y };
+        }
     }
 
     public class Pipe
     {
-        public PipeVector2 Location;
-        public Dictionary<PipeVector2, Pipe?> Connections = new Dictionary<PipeVector2, Pipe?>();
+        public PipeVector2 Location { get; set; }
+        public PipeDirection Direction {  get; set; }
+        public Dictionary<PipeVector2, Pipe?> Connections { get; set; } = new Dictionary<PipeVector2, Pipe?>();
     }
 
     public class Solution : IAOCSolution
@@ -132,13 +149,13 @@ namespace AdventOfCode2023Solutions.Day10
         public string[] DatasetLines { get; }
 
         private Pipe StartPipe { get; }
-        public List<Pipe> PipeLine { get; }
+        public List<Pipe> Pipeline { get; }
 
         public Solution(string[] datasetLines)
         {
             DatasetLines = datasetLines;
             StartPipe = GetStartPipe();
-            PipeLine = new List<Pipe>() { StartPipe };
+            Pipeline = new List<Pipe>() { StartPipe };
         }
 
         public string SolvePart1()
@@ -147,12 +164,12 @@ namespace AdventOfCode2023Solutions.Day10
             
             while (StartPipe != nextPipe)
             {
-                PipeLine.Add(nextPipe);
-                nextPipe = GetNextPipe(PipeLine[PipeLine.Count - 1]);
+                Pipeline.Add(nextPipe);
+                nextPipe = GetNextPipe(Pipeline[Pipeline.Count - 1]);
                 
             }
             
-            int furthestDistance = (int)(PipeLine.Count * 0.5);
+            int furthestDistance = (int)(Pipeline.Count * 0.5);
 
 
             return furthestDistance.ToString();
@@ -160,7 +177,18 @@ namespace AdventOfCode2023Solutions.Day10
 
         public string SolvePart2()
         {
-            return "To be implemented";
+            if (2 > Pipeline.Count) { SolvePart1(); }
+
+            FrozenDictionary<PipeVector2,Pipe> pipeRing = Pipeline.ToFrozenDictionary(a=>a.Location, a=>a);
+            List<PipeVector2> inside = [];
+
+            PipeVector2 searchStartPipeLocation = SearchRingStartPipe(pipeRing, out PipeVector2 searchDirection);
+
+            AddInsideAlongPipeline(searchStartPipeLocation, searchDirection, pipeRing, inside);
+
+            AddFloatingInside(inside, pipeRing);
+
+            return inside.Count.ToString();
         }
 
         private Pipe GetStartPipe()
@@ -176,28 +204,28 @@ namespace AdventOfCode2023Solutions.Day10
                     start = new Pipe() { Location = new PipeVector2() { X = startX, Y = i } };
 
                     Pipe? north = GetPipeFrom(start.Location + PipeConnections.North);
-                    if (null != north) 
+                    if (null != north && PipeDirection.South.HasFlag(north.Direction)) 
                     {
                         north.Connections[PipeConnections.South] = start;
                         start.Connections[PipeConnections.North] = north;
                     }
 
                     Pipe? south = GetPipeFrom(start.Location + PipeConnections.South);
-                    if (null != south)
+                    if (null != south && PipeDirection.North.HasFlag(south.Direction))
                     {
                         south.Connections[PipeConnections.North] = start;
                         start.Connections[PipeConnections.South] = south;
                     }
 
                     Pipe? east = GetPipeFrom(start.Location + PipeConnections.East);
-                    if (null != east)
+                    if (null != east && PipeDirection.West.HasFlag(east.Direction))
                     {
                         east.Connections[PipeConnections.West] = start;
                         start.Connections[PipeConnections.East] = east;
                     }
 
                     Pipe? west = GetPipeFrom(start.Location + PipeConnections.West);
-                    if (null != west)
+                    if (null != west && PipeDirection.East.HasFlag(west.Direction))
                     {
                         west.Connections[PipeConnections.East] = start;
                         start.Connections[PipeConnections.West] = west;
@@ -220,14 +248,21 @@ namespace AdventOfCode2023Solutions.Day10
             {
                 if(null == keyValuePair.Value)
                 {
-                    if (StartPipe.Location == currentPipe.Location + keyValuePair.Key) { return StartPipe; }
-                    nextPipe = GetPipeFrom(currentPipe.Location + keyValuePair.Key);
+                    if (StartPipe.Location == currentPipe.Location + keyValuePair.Key) 
+                    {
+                        nextPipe = StartPipe;
+                    }
+                    else
+                    {
+                        nextPipe = GetPipeFrom(currentPipe.Location + keyValuePair.Key);
+                    }
+
                     currentPipe.Connections[keyValuePair.Key] = nextPipe;
+
                     if (null != nextPipe)
                     {
                         nextPipe.Connections[-keyValuePair.Key] = currentPipe;
-                    }
-                        
+                    }  
                 }
             }
 
@@ -251,10 +286,103 @@ namespace AdventOfCode2023Solutions.Day10
                 return new Pipe()
                 {
                     Location = location,
+                    Direction = PipeConnections.PipeDirectionFromChar[DatasetLines[location.Y][location.X]],
                     Connections = PipeConnections.ConnectionsFromDirection(PipeConnections.PipeDirectionFromChar[DatasetLines[location.Y][location.X]])
                 };
             }
             else { return null; }
         }
+
+        private PipeVector2 SearchForPipeline(FrozenDictionary<PipeVector2, Pipe> pipeRing, out PipeVector2 searchLocation)
+        {
+            PipeVector2 lastLocation = new PipeVector2() { X = -1, Y = -1 };
+            
+            for (int iY = 0; iY < DatasetLines.Length; iY++)
+            {
+                lastLocation = new PipeVector2() { X = -1, Y = iY };
+
+                for (int iX = 0; iX < DatasetLines[iY].Length; iX++)
+                {
+                    PipeVector2 iLocation = new PipeVector2() { X = iX, Y = iY };
+
+                    if (pipeRing.ContainsKey(iLocation))
+                    {
+                        searchLocation = lastLocation;
+                        return iLocation;
+                    }
+
+                    lastLocation = iLocation;
+                }
+
+            }
+
+            throw new Exception("No PipeLine found");
+        }
+
+        private PipeVector2 SearchRingStartPipe(FrozenDictionary<PipeVector2, Pipe> pipeRing, out PipeVector2 searchDirection)
+        {
+            PipeVector2 pipelineLocation = SearchForPipeline(pipeRing, out PipeVector2 searchLocation);
+
+            PipeVector2 circlePipeDirection = (pipelineLocation - searchLocation).Right90();
+
+            while (!pipeRing.ContainsKey(searchLocation) || !pipeRing[searchLocation].Connections.ContainsKey(pipelineLocation - searchLocation))
+            {
+                searchLocation = searchLocation + circlePipeDirection;
+                circlePipeDirection = circlePipeDirection.Left90();
+                searchLocation = searchLocation + circlePipeDirection;
+            }
+            
+            searchDirection = pipelineLocation - searchLocation;
+            return searchLocation;
+            
+        }
+
+        private void AddInsideAlongPipeline(PipeVector2 currentPipeLocation, PipeVector2 searchDirection, FrozenDictionary<PipeVector2, Pipe> pipeRing, List<PipeVector2> inside)
+        {
+            for (int iRing = 0; iRing <= pipeRing.Count; iRing++)
+            {
+                // search right of current
+                AddInside(currentPipeLocation + searchDirection.Right90(), pipeRing, inside);
+                
+                // search right of next
+                if (null != pipeRing[currentPipeLocation].Connections[searchDirection]) 
+                {
+                    currentPipeLocation = pipeRing[currentPipeLocation].Connections[searchDirection].Location;
+                }
+                else { throw new Exception("Pipeline broken"); }
+                AddInside(currentPipeLocation + searchDirection.Right90(), pipeRing, inside);
+
+                // turn
+                foreach (PipeVector2 direction in pipeRing[currentPipeLocation].Connections.Keys) 
+                {
+                    if (direction != searchDirection.Back180())
+                    {
+                        searchDirection = direction;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void AddFloatingInside(List<PipeVector2> inside, FrozenDictionary<PipeVector2, Pipe> pipeRing)
+        {
+            PipeVector2[] searchLocations = [PipeConnections.North, PipeConnections.South, PipeConnections.East, PipeConnections.West];
+            for (int i = 0; i < inside.Count; i++)
+            {
+                foreach (PipeVector2 searchLocation in searchLocations)
+                {
+                    AddInside(inside[i] + searchLocation, pipeRing, inside);
+                }
+            }
+        }
+
+        private void AddInside(PipeVector2 searchLocation, FrozenDictionary<PipeVector2, Pipe> pipeRing, List<PipeVector2> inside)
+        {
+            if (!pipeRing.ContainsKey(searchLocation) && !inside.Contains(searchLocation))
+            {
+                inside.Add(searchLocation);
+            }
+        }
+
     }
 }
