38e18e32afe2461083a27dcd5a479a3b75c997c7
Telpe <tommy.s.madsen@gmail.com>
2024-12-01 09:10:21 +0100

    Day 12 packed up, may be continued later.

--- AdventOfCode2023Solutions/Day12/Solution.cs
+++ AdventOfCode2023Solutions/Day12/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using Common;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -10,39 +11,115 @@ namespace AdventOfCode2023Solutions.Day12
 
         public string SolvePart1(string[] datasetLines)
         {
-            SpringStatusSheet[] springStatusSheets = BuildSpringStatusSheets(datasetLines);
+            /*
+            SpringStatusSheet3[] springStatusSheets = BuildSpringStatusSheets(datasetLines);
 
-            int sumArrangements = 0;
-            int sumArrangements2 = 0;
+            long sumArrangements = 0;
 
-            foreach (SpringStatusSheet sheet in springStatusSheets)
+            foreach (SpringStatusSheet3 sheet in springStatusSheets)
             {
                 sumArrangements += sheet.ArrangementsCount;
-                sumArrangements2 += sheet.Arrangements.Count;
             }
 
-            return sumArrangements2.ToString();
+            return sumArrangements.ToString();
+            */
+            return "";
         }
 
         public string SolvePart2(string[] datasetLines)
         {
-            return "To be implemented";
+            /*
+            SpringStatusSheet2[] springStatusSheets = BuildSpringStatusSheets2(datasetLines);
+            
+            long sumArrangements = 0;
+
+            //int x = 1;
+            
+            foreach (SpringStatusSheet2 sheet in springStatusSheets)
+            {
+                sumArrangements += sheet.ArrangementsCount;
+                //x++;
+            }
+
+            return sumArrangements.ToString();
+            */
+            return "";
         }
 
-        private SpringStatusSheet[] BuildSpringStatusSheets(string[] datasetLines)
+        /*
+        private SpringStatusSheet3[] BuildSpringStatusSheets(string[] datasetLines)
         {
-            SpringStatusSheet[] result = new SpringStatusSheet[datasetLines.Length];
+            int simultanousTasks = 3;
+            SpringStatusSheet3[] result = Array.Empty<SpringStatusSheet3>();
+
+            int tasksSize = (int)Math.Ceiling((double)datasetLines.Length / simultanousTasks);
+            Task<SpringStatusSheet3[]>[] tasks = new Task<SpringStatusSheet3[]>[tasksSize];
+
+            for (int tasksIndex = 0; tasksIndex < tasksSize; tasksIndex++)
+            {
+                int maxLineIndex = tasksSize * (tasksIndex + 1);
+                maxLineIndex = datasetLines.Length < maxLineIndex ? datasetLines.Length : maxLineIndex;
+
+                tasks[tasksIndex] = Task.Run(() =>
+                {
+                    int minLineIndex = tasksSize * tasksIndex;
+                    SpringStatusSheet3[] taskResult = new SpringStatusSheet3[maxLineIndex - minLineIndex];
+                    int taskResultIndex = 0;
+
+                    for (int datasetLinesIndex = minLineIndex; datasetLinesIndex < maxLineIndex; datasetLinesIndex++)
+                    {
+                        string[] data = datasetLines[datasetLinesIndex].Split(' ');
+                        taskResult[taskResultIndex] = new SpringStatusSheet3(data[0], data[1]);
+
+                        taskResultIndex++;
+                    }
+                    return taskResult;
+                });
+
+            }
+
+            Task.WaitAll(tasks);
+
+            int resultLength = 0;
+            foreach (var task in tasks) 
+            {
+                resultLength += task.Result.Length;
+            }
+
+            result = new SpringStatusSheet3[resultLength];
+            int resultIndex = 0;
+
+            foreach (var task in tasks)
+            {
+                for (int i = 0; i < task.Result.Length; i++, resultIndex++)
+                {
+                    result[resultIndex] = task.Result[i];
+                }
+            }
+
+            return result;
+        }
+        */
+
+        /*
+        private SpringStatusSheet2[] BuildSpringStatusSheets2(string[] datasetLines)
+        {
+            SpringStatusSheet2[] result = new SpringStatusSheet2[datasetLines.Length];
 
             for (int i = 0; i < datasetLines.Length; i++)
             {
                 string[] data = datasetLines[i].Split(' ');
-                DamagedGroup[] damagedGroups = data[1].Split(',').Select(a=> new DamagedGroup(int.Parse(a)) ).ToArray();
+                Memory3[] damagedGroups = UnfoldFives(data[1], ',').Split(',').Select(a => new Memory3(int.Parse(a))).ToArray();
 
-                result[i] = (new SpringStatusSheet(data[0],damagedGroups));
+                result[i] = (new SpringStatusSheet2(UnfoldFives(data[0], '?'), damagedGroups));
             }
 
-
             return result;
+        }
+        */
+        private string UnfoldFives(string toRepeat, char separator)
+        {
+            return String.Join(separator, Enumerable.Repeat(toRepeat, 5));
         }
 
     }
