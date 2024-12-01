61ba04dec167adf1f029a6354d584a52457bdff7
Flemming Madsen <f.m.madsen@live.dk>
2024-12-01 01:50:44 +0100

    Addedd 2025 AOC code projects

---
+++ AdventOfCode2025Solutions/Day05/Solution.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+
+namespace AdventOfCode2025Solutions.Day05
+{
+    public class Solution : IAOCSolution
+    {
+        public string PuzzleName => "Day 5: ";
+
+        public string SolvePart1(string[] datasetLines)
+        {
+            return "To be implemented";
+        }
+
+        public string SolvePart2(string[] datasetLines)
+        {
+            return "To be implemented";
+        }
+    }
+}
---
+++ AdventOfCode2025Solutions/Day21/Solution.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+
+namespace AdventOfCode2025Solutions.Day21
+{
+    public class Solution : IAOCSolution
+    {
+        public string PuzzleName => "Day 21: ";
+
+        public string SolvePart1(string[] datasetLines)
+        {
+            return "To be implemented";
+        }
+
+        public string SolvePart2(string[] datasetLines)
+        {
+            return "To be implemented";
+        }
+    }
+}
