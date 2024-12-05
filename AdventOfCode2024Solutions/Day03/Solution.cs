2b6ea2f68bbb246263cac1b40efbbd1962a643d0
Telpe <tommy.s.madsen@gmail.com>
2024-12-05 20:16:10 +0100

    Day 3 done

--- AdventOfCode2024Solutions/Day03/Solution.cs
+++ AdventOfCode2024Solutions/Day03/Solution.cs
@@ -1,24 +1,108 @@
 ﻿using Common;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AdventOfCode2024Solutions.Day03
 {
     public class Solution : IAOCSolution
     {
         public string PuzzleName => "Day 3: ";
+        protected static readonly string MulPattern = @"mul\((\d+),(\d+)\)";
+        protected static readonly string DoPattern = @"(?<method>mul)\((?<mulleft>\d+),(?<mulright>\d+)\)|(?<method>don)'t\(\)";
+        protected static readonly string DontPattern = @"(?<method>do)\(\)";
+        protected static readonly Regex MulFinder = new Regex(MulPattern);
+        protected static readonly Regex DoFinder = new Regex(DoPattern);
+        protected static readonly Regex DontFinder = new Regex(DontPattern);
 
         public string SolvePart1(string[] datasetLines)
         {
-            foreach (var datasetLine in datasetLines)
-            {
+            int total = 0;
 
+            foreach (string datasetLine in datasetLines)
+            {
+                MatchCollection mulMatches = MatchMul(datasetLine);
+
+                foreach (Match match in mulMatches)
+                {
+                    total += Mul(match);
+                }
             }
 
-            return "To be implemented";
+            return total.ToString();
         }
 
         public string SolvePart2(string[] datasetLines)
         {
-            return "To be implemented";
+            int total = 0;
+            var searchMode = DoMode;
+
+            foreach (string datasetLine in datasetLines)
+            {
+                int lineIndex = 0;
+
+                while (lineIndex < datasetLine.Length)
+                {
+                    Match match = (Match?)searchMode.DynamicInvoke(datasetLine, lineIndex) ?? Match.Empty;
+
+                    if (!match.Success) { lineIndex = datasetLine.Length; }
+                    else
+                    {
+                        match.Groups.TryGetValue("method", out Group? method);
+
+                        if (null != method)
+                        {
+                            switch (method.Value)
+                            {
+                                case "mul":
+                                    match.Groups.TryGetValue("mulleft", out Group? mulleft);
+                                    match.Groups.TryGetValue("mulright", out Group? mulright);
+                                    if (null != mulleft && null != mulright)
+                                    {
+                                        total += Int32.Parse(mulleft.Value) * Int32.Parse(mulright.Value);
+                                    }
+
+                                    break;
+
+                                case "do":
+                                    searchMode = DoMode;
+                                    break;
+
+                                case "don":
+                                    searchMode = DontMode;
+                                    break;
+                            }
+                        }
+
+                        lineIndex = match.Index + match.Length;
+                    }
+                    
+                }
+
+            }
+
+            return total.ToString();
+        }
+
+        protected static int Mul(Match match)
+        {
+            GroupCollection groups = match.Groups;
+            return Int32.Parse(groups[1].Value) * Int32.Parse(groups[2].Value);
+        }
+
+        protected static MatchCollection MatchMul(string text)
+        {
+            return MulFinder.Matches(text);
+        }
+
+        protected static Match DoMode(string datasetLine, int lineIndex)
+        {
+            return DoFinder.Match(datasetLine, lineIndex);
+        }
+
+        protected static Match DontMode(string datasetLine, int lineIndex)
+        {
+            return DontFinder.Match(datasetLine, lineIndex);
         }
     }
 }
--- AdventOfCode2024UnitTests/Day03Tests.cs
+++ AdventOfCode2024UnitTests/Day03Tests.cs
@@ -20,14 +20,14 @@ namespace AdventOfCode2024UnitTests
         public void Part2()
         {
             //Prepare
-            var dataset = TestDataReader.ReadDataSet("TestDataSetDay03.txt");
+            var dataset = TestDataReader.ReadDataSet("TestDataSetDay03Part2.txt");
             var solution = new AdventOfCode2024Solutions.Day03.Solution();
 
             //act
             var result = solution.SolvePart2(dataset);
 
             //assert
-            Assert.That(result, Is.EqualTo(""));
+            Assert.That(result, Is.EqualTo("48"));
         }
     }
 }
