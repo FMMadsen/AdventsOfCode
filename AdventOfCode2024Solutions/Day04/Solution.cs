1dd9bbc83627bfdec7a2395975fedbfbff9e1127
Telpe <tommy.s.madsen@gmail.com>
2024-12-06 17:58:10 +0100

    Day 4 Part 1 done

--- AdventOfCode2024Solutions/Day04/Solution.cs
+++ AdventOfCode2024Solutions/Day04/Solution.cs
@@ -1,14 +1,151 @@
 ﻿using Common;
+using System.Numerics;
 
 namespace AdventOfCode2024Solutions.Day04
 {
     public class Solution : IAOCSolution
     {
         public string PuzzleName => "Day 4: ";
+        public readonly string SearchWord = "XMAS";
+        protected Vector2I Maximum = new Vector2I();
+        protected string[] Matrix = Array.Empty<string>();
 
         public string SolvePart1(string[] datasetLines)
         {
-            return "To be implemented";
+            Matrix = datasetLines;
+            Maximum = new Vector2I(Matrix[0].Length - 1, Matrix.Length - 1);
+
+            int count = 0;
+
+            int lineNumber = 0;
+            int lineIndex = 0;
+
+            while (lineNumber < Matrix.Length) 
+            {
+                while (lineIndex < Matrix[0].Length)
+                {
+                    count += CheckForWord(new Vector2I(lineIndex, lineNumber));
+
+                    lineIndex++;
+                }
+
+                lineIndex = 0;
+                lineNumber++;
+            }
+
+            return count.ToString();
+        }
+
+        protected int CheckForWord(Vector2I position)
+        {
+            int count = 0;
+            int wordIndex = 0;
+
+            if (1 < SearchWord.Length && IsCharInPosition(SearchWord[wordIndex], position))
+            {
+                wordIndex++;
+
+                bool canGoNorth = SearchWord.Length -1 <= position.Y;
+                bool canGoEast = position.X <= Maximum.X - (SearchWord.Length - 1);
+                bool canGoSouth = position.Y <= Maximum.Y - (SearchWord.Length - 1);
+                bool canGoWest = SearchWord.Length - 1 <= position.X;
+
+                if (canGoNorth) 
+                {
+                    if (CheckDirection(Vector2I.NN, wordIndex, position))
+                    {
+                        count++;
+                    }
+
+                    if (canGoWest)
+                    {
+                        if (CheckDirection(Vector2I.NW, wordIndex, position))
+                        {
+                            count++;
+                        }
+                    }
+
+                    if (canGoEast)
+                    {
+                        if (CheckDirection(Vector2I.NE, wordIndex, position))
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                if (canGoSouth)
+                {
+                    if (CheckDirection(Vector2I.SS, wordIndex, position))
+                    {
+                        count++;
+                    }
+
+                    if (canGoWest)
+                    {
+                        if (CheckDirection(Vector2I.SW, wordIndex, position))
+                        {
+                            count++;
+                        }
+                    }
+
+                    if (canGoEast)
+                    {
+                        if (CheckDirection(Vector2I.SE, wordIndex, position))
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                if (canGoWest)
+                {
+                    if (CheckDirection(Vector2I.WW, wordIndex, position))
+                    {
+                        count++;
+                    }
+                }
+
+                if (canGoEast)
+                {
+                    if (CheckDirection(Vector2I.EE, wordIndex, position))
+                    {
+                        count++;
+                    }
+                }
+            }
+            else if(IsCharInPosition(SearchWord[wordIndex], position))
+            {
+                count++;
+            }
+                
+            return count;
+        }
+
+        protected bool CheckDirection(Vector2I direction, int wordIndex, Vector2I startPosition)
+        {
+            bool success = false;
+            Vector2I distance = wordIndex * direction;
+            Vector2I searchPos = startPosition + (wordIndex * direction);
+
+            if (IsCharInPosition(SearchWord[wordIndex], startPosition + (wordIndex * direction)))
+            {
+                if (wordIndex + 1 < SearchWord.Length)
+                {
+                    success = CheckDirection(direction, wordIndex + 1, startPosition);
+                }
+                else
+                {
+                    success = true;
+                }
+            }
+
+            return success;
+        }
+
+        protected bool IsCharInPosition(char aChar, Vector2I index)
+        {
+            return Matrix[index.Y][index.X] == aChar;
         }
 
 
---
+++ AdventOfCode2024Solutions/Day04/Vector2I.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024Solutions.Day04
+{
+    public struct Vector2I
+    {
+        public int X = 0;
+        public int Y = 0;
+
+        public Vector2I()
+        {
+
+        }
+
+        public Vector2I(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector2I NN { get { return new Vector2I(0, -1); } }
+        public static Vector2I NE { get { return new Vector2I(1, -1); } }
+        public static Vector2I EE { get { return new Vector2I(1, 0); } }
+        public static Vector2I SE { get { return new Vector2I(1, 1); } }
+        public static Vector2I SS { get { return new Vector2I(0, 1); } }
+        public static Vector2I SW { get { return new Vector2I(-1, 1); } }
+        public static Vector2I WW { get { return new Vector2I(-1, 0); } }
+        public static Vector2I NW { get { return new Vector2I(-1, -1); } }
+
+        public static Vector2I operator +(Vector2I left , Vector2I right)
+        {
+            return new Vector2I(left.X + right.X, left.Y + right.Y);
+        }
+
+        public static Vector2I operator -(Vector2I left, Vector2I right)
+        {
+            return new Vector2I(left.X - right.X, left.Y - right.Y);
+        }
+
+        public static Vector2I operator *(Vector2I left, Vector2I right)
+        {
+            return new Vector2I(left.X * right.X, left.Y * right.Y);
+        }
+
+        public static Vector2I operator *(Vector2I left, int right)
+        {
+            return new Vector2I(left.X * right, left.Y * right);
+        }
+
+        public static Vector2I operator *(int left, Vector2I right)
+        {
+            return new Vector2I(left * right.X, left * right.Y);
+        }
+    }
+}
