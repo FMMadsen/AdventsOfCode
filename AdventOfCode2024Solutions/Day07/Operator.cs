81b847aa9b5a43027e1a6e2dd342ecb638699553
Telpe <tommy.s.madsen@gmail.com>
2024-12-15 11:26:43 +0100

    Day 7 done

---
+++ AdventOfCode2024Solutions/Day07/Operator.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024Solutions.Day07
+{
+    public class Operator
+    {
+        public long TestValue { get; set; }
+        public int[]  Numbers { get; set; }
+
+        protected bool CombinationFoundValue = false;
+        public bool CombinationFound { get { return CombinationFoundValue; } }
+        protected bool TestedValue = false; 
+        public bool Tested { get { return TestedValue; } }
+
+        protected OperatorEnum[][] CombinationsValue = Array.Empty<OperatorEnum[]>();
+        protected int CombinationsIndex = 0;
+        protected OperatorEnum[] Options = [OperatorEnum.Add, OperatorEnum.Multiply];
+        protected OperatorEnum[] Options2 = [OperatorEnum.Concat, OperatorEnum.Add, OperatorEnum.Multiply];
+        protected OperatorEnum[] CurrentCombo = Array.Empty<OperatorEnum>();
+
+        public Operator() 
+        {
+            TestValue = -1;
+            Numbers = Array.Empty<int>();
+        }
+
+        public bool TestCombinations(bool use2 = false)
+        {
+            // fill combinatins first
+            if (use2)
+            {
+                CombinationsValue = new OperatorEnum[(int)Math.Pow(Options2.Length, Numbers.Length - 1)][];
+                CurrentCombo = new OperatorEnum[Numbers.Length - 1];
+                FillCombo(CurrentCombo.Length - 1, Options2);
+            }
+            else
+            {
+                CombinationsValue = new OperatorEnum[(int)Math.Pow(Options.Length, Numbers.Length - 1)][];
+                CurrentCombo = new OperatorEnum[Numbers.Length - 1];
+                FillCombo(CurrentCombo.Length - 1, Options);
+            }
+            
+
+            // test combinations
+            foreach (OperatorEnum[] combo in CombinationsValue)
+            {
+                if (TestCombo(combo))
+                {
+                    CombinationFoundValue = true;
+                    break;
+                }
+            }
+
+
+            TestedValue = true;
+
+            return CombinationFound;
+        }
+
+        protected void FillCombo(int comboIndex, OperatorEnum[] options)
+        {
+            for(int optionsIndex = options.Length-1; -1 < optionsIndex; optionsIndex--)
+            {
+                CurrentCombo[comboIndex] = options[optionsIndex];
+
+                if (0 < comboIndex)
+                {
+                    FillCombo(comboIndex-1, options);
+                }
+                else
+                {
+                    CombinationsValue[CombinationsIndex] = (OperatorEnum[])CurrentCombo.Clone();
+                    CombinationsIndex++;
+                }
+            }
+        }
+
+        protected bool TestCombo(OperatorEnum[] aCombo)
+        {
+            long result = Numbers[0];
+
+            for (int i = 0; i < aCombo.Length; i++)
+            {
+                if (OperatorEnum.Multiply == aCombo[i])
+                {
+                    result *= Numbers[i + 1];
+                }
+                else if(OperatorEnum.Add == aCombo[i])
+                {
+                    result += Numbers[i + 1];
+                }
+                else if (OperatorEnum.Concat == aCombo[i])
+                {
+                    result = Int64.Parse(result.ToString() + Numbers[i + 1].ToString());
+                }
+
+                if (TestValue < result)
+                {
+                    return false;
+                }
+            }
+
+            if (TestValue == result)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
--- AdventOfCode2024Solutions/Day07/Solution.cs
+++ AdventOfCode2024Solutions/Day07/Solution.cs
@@ -8,12 +8,55 @@ namespace AdventOfCode2024Solutions.Day07
 
         public string SolvePart1(string[] datasetLines)
         {
-            return "To be implemented";
+            // for each succes
+            // total += number
+            long count = 0;
+
+            foreach (var datasetLine in datasetLines)
+            {
+                long testValue;
+                int[] numbersList;
+                {
+                    string[] temp = datasetLine.Split(':');
+                    testValue = Int64.Parse(temp[0]);
+                    numbersList = temp[1].Split(' ', StringSplitOptions.RemoveEmptyEntries|StringSplitOptions.TrimEntries).Select(a=> Int32.Parse(a)).ToArray();
+                }
+
+                Operator currentOperatorString = new() { TestValue = testValue, Numbers = numbersList };
+                if (currentOperatorString.TestCombinations())
+                {
+                    count += currentOperatorString.TestValue;
+                }
+                
+            }
+
+            return count.ToString();
         }
+
 
         public string SolvePart2(string[] datasetLines)
         {
-            return "To be implemented";
+            long count = 0;
+
+            foreach (var datasetLine in datasetLines)
+            {
+                long testValue;
+                int[] numbersList;
+                {
+                    string[] temp = datasetLine.Split(':');
+                    testValue = Int64.Parse(temp[0]);
+                    numbersList = temp[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(a => Int32.Parse(a)).ToArray();
+                }
+
+                Operator currentOperatorString = new() { TestValue = testValue, Numbers = numbersList };
+                if (currentOperatorString.TestCombinations(true))
+                {
+                    count += currentOperatorString.TestValue;
+                }
+
+            }
+
+            return count.ToString();
         }
     }
 }
