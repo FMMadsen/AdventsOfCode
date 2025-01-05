a0fb2cf7caf17cf84e356fafd2a09a3540824de6
Flemming Madsen <f.m.madsen@live.dk>
2025-01-05 15:23:18 +0100

    Improved Day 8 Part 1

---
+++ AdventOfCode2024Solutions/Day08/AntennaMap.cs
@@ -0,0 +1,148 @@
+﻿using ToolsFramework;
+
+namespace AdventOfCode2024Solutions.Day08
+{
+    internal class AntennaMap
+    {
+        private readonly Dictionary<char, List<Coordinate>> antennas = [];
+        private int numberOfXs = 0;
+        private int numberOfYs = 0;
+
+        private char[,]? antinodeMap;
+
+        internal int AntinodeCount => antinodeMap?.Length ?? 0;
+
+        public AntennaMap(string[] mapLines)
+        {
+            numberOfYs = mapLines.Length;
+            numberOfXs = mapLines[0].Length;
+
+            if (Solution.PrintFinalMapToConsole || Solution.PrintInitialMapToConsole || Solution.PrintMapToConsoleEveryStep)
+                antinodeMap = new char[numberOfXs, numberOfYs];
+
+            for (int y = 0; y < mapLines.Length; y++)
+            {
+                for (int x = 0; x < mapLines[y].Length; x++)
+                {
+                    var mapSpace = mapLines[y][x];
+
+                    AddToAntinodeMap(mapSpace, x, y);
+
+                    if (mapSpace == '.')
+                        continue;
+
+                    var antennaCoordinate = new Coordinate(x, y);
+
+                    if (antennas.TryGetValue(mapSpace, out var aList))
+                        aList.Add(antennaCoordinate);
+                    else
+                        antennas.Add(mapSpace, new List<Coordinate> { antennaCoordinate });
+                }
+            }
+
+            if (Solution.PrintInitialMapToConsole)
+                ConsolePrinting.PrintMapToConsole(antinodeMap);
+        }
+
+        internal int IdentifyAndCountAntinodes(bool extendedResonance = false)
+        {
+            var antinodes = new HashSet<int>();
+
+            foreach (var frequency in antennas.Keys)
+            {
+                IdentifyAntinodes(antennas[frequency].ToArray(), antinodes, extendedResonance);
+                if (Solution.PrintMapToConsoleEveryStep)
+                    ConsolePrinting.PrintMapToConsole(antinodeMap);
+            }
+
+            if (Solution.PrintFinalMapToConsole)
+                ConsolePrinting.PrintMapToConsole(antinodeMap);
+
+            return antinodes.Count;
+        }
+
+        private void IdentifyAntinodes(Coordinate[] coordinates, HashSet<int> antinodes, bool extendedResonance)
+        {
+            var numberOfCoordinates = coordinates.Length;
+            for (int a = 0; a < numberOfCoordinates - 1; a++)
+                for (int b = a + 1; b < numberOfCoordinates; b++)
+                {
+                    CalculateAntinodes(coordinates[a], coordinates[b], antinodes);
+                    if (Solution.PrintMapToConsoleEveryStep)
+                        ConsolePrinting.PrintMapToConsole(antinodeMap);
+                }
+        }
+
+        private void CalculateAntinodes(Coordinate a, Coordinate b, HashSet<int> antinodes)
+        {
+            CalculateAntinodeExtendRight(a, b, antinodes);
+            CalculateAntinodeExtendLeft(a, b, antinodes);
+        }
+
+        private void CalculateAntinodeExtendRight(Coordinate a, Coordinate b, HashSet<int> antinodes)
+        {
+            var antinodeX = b.X + (b.X - a.X);
+            var antinodeY = b.Y + (b.Y - a.Y);
+            var newCoordinate = new Coordinate(antinodeX, antinodeY);
+            var isWithinMap = IsCoordinateInMap(newCoordinate);
+
+            if (isWithinMap)
+            {
+                AddToAntinodeMap('#', newCoordinate);
+                antinodes.Add(CreateCoordinateKey(newCoordinate));
+            }
+        }
+
+        private void CalculateAntinodeExtendLeft(Coordinate a, Coordinate b, HashSet<int> antinodes)
+        {
+            var antinodeX = a.X - (b.X - a.X);
+            var antinodeY = a.Y - (b.Y - a.Y);
+            var newCoordinate = new Coordinate(antinodeX, antinodeY);
+            var isWithinMap = IsCoordinateInMap(newCoordinate);
+
+            if (isWithinMap)
+            {
+                AddToAntinodeMap('#', newCoordinate);
+                antinodes.Add(CreateCoordinateKey(newCoordinate));
+            }
+        }
+
+        private int CreateCoordinateKey(Coordinate coordinate)
+        {
+            return coordinate.X * 100 + coordinate.Y;
+        }
+
+        private int CreateCoordinateKey(int x, int y)
+        {
+            return x * 100 + y;
+        }
+
+        private void AddToAntinodeMap(char mapTile, Coordinate coordinate)
+        {
+            if (antinodeMap == null)
+                return;
+
+            if (IsCoordinateInMap(coordinate))
+                antinodeMap[coordinate.Y, coordinate.X] = mapTile;
+        }
+
+        private void AddToAntinodeMap(char mapTile, int x, int y)
+        {
+            if (antinodeMap == null)
+                return;
+
+            if (IsCoordinateInMap(x, y))
+                antinodeMap[y, x] = mapTile;
+        }
+
+        private bool IsCoordinateInMap(Coordinate coordinate)
+        {
+            return coordinate.X >= 0 && coordinate.X < numberOfXs && coordinate.Y >= 0 && coordinate.Y < numberOfYs;
+        }
+
+        private bool IsCoordinateInMap(int x, int y)
+        {
+            return x >= 0 && x < numberOfXs && y >= 0 && y < numberOfYs;
+        }
+    }
+}
